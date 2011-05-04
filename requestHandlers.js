5a21e5217868c3eb05e629630fbf283cc6e776a4
James Bloomer <github@thebloomers.co.uk>
2011-05-04 06:10:22 +0000

    Example of a using exec (doesn't work on public Cloud9 repo)

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,13 +1,15 @@
+var exec = require("child_process").exec;
+
 function start() {
     console.log("Request handler 'start' was called.");
     
-    function sleep(milliSeconds) {
-        var startTime = new Date().getTime(); // get the current time
-        while (new Date().getTime() < startTime + milliSeconds); // hog cpu
-      }
-      
-    sleep(10000);  
-    return "Hello start";
+    var content = "empty";
+    
+    exec("ls -lah", function (error, stdout, stderr) {
+        content = stdout;
+    });
+    
+    return content;
 }
 
 function upload() {
