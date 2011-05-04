4bffb774c31f6d4bbc5b5567193e3a92aed7a5d7
James Bloomer <github@thebloomers.co.uk>
2011-05-04 05:56:57 +0000

    How not to do it

--- router.js
+++ router.js
@@ -1,8 +1,9 @@
 function route(handle, pathname) {
     if (typeof handle[pathname] === "function") {
-        handle[pathname]();
+        return handle[pathname]();
     } else {
         console.log("No request handler found for " + pathname);
+        return "404 Not Found";
     };
     
     console.log("About to route a request for " + pathname);
